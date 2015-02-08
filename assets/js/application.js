5135cc41195641952ef270360390dea25b9614ec
Felipe Safra <felipesafra@hotmail.com>
2015-02-08 18:11:40 -0200

    Alteracoes: remocao do plugin inputMask, criado método de máscara

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,32 +1,50 @@
 'use strict'
 
+function maskFormat(campo_passado, mascara) {
+	var campo = campo_passado.value.length;
+	var saida = mascara.substring(0,1);
+	var texto = mascara.substring(campo);
+
+	if(texto.substring(0,1) != saida) {
+		campo_passado.value += texto.substring(0,1);
+	}
+};
+
 function renderSmartImput(options) {
 	var _options = options || {};
 	_options.mask = options.mask ;
 	_options.regex = options.regex;
+	var el = document.getElementById('texto');
 
-	var el = $('#texto');
-	el.on("keyup", function() {
-		var str = el.val();
-		var patt = new RegExp(_options.regex);
-		var validacao = patt.test(str);
-		if(validacao){
-			el.before('<span class="success message">Obrigado por preencher o campo corretamente.</span>');
-			el.addClass('success');
-
-		} else {
-			el.before('<span class="error message">Por favor, preencha o campo corretamente.</span>');
-			el.addClass('error');
-			 $(".message").fadeOut(1000);
-		}
+	$(el).attr('maxlength', _options.mask.length);
+	
+	$(el).keypress(function() {
+		maskFormat(el, _options.mask);
 	});
 
-	el.inputmask(_options.mask);
+	$(el).keyup(function() {
+		var validacao = el.value.match(_options.regex);
+	
+		if(el.value.length == _options.mask.length && validacao) {
+			$(el).before('<span class="success message">Obrigado por preencher o campo corretamente.</span>');
+			$(el).addClass('success');
+			if($('.error.message').length) {
+				$('.error.message').hide();
+			}
+		} else if(el.value.length == _options.mask.length && !validacao) {
+			$(el).before('<span class="error message">Por favor, preencha o campo corretamente.</span>');
+			$(el).addClass('error');
+			if($('.success.message').length) {
+				$('.success.message').hide();
+			}
+		}
+
+	});
 };
 
 $(document).ready(function() {
 	renderSmartImput({
 		mask: '9999 9999 9999 9999',
-		regex: "([0-9]{4})( [0-9]{4})( [0-9]{4})( [0-9]{4})"
+		regex: /(?:[\d]{4}\s){3}[\d]{4}/g
 	});
 });
